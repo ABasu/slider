b3b0e5943da8b22ca14e0ddc9b5f0a40bdca9d76
Anupam Basu <prime.lens@gmail.com>
2012-11-14 02:45:37 -0600

    Version 1

---
+++ README.md
@@ -0,0 +1,47 @@
+slider.py - A Bare-bones Slideshow Generator
+============================================
+
+Slider lets you connect individual images, markdown files and html documents into a slide show. Slides can be as simple as a single image or some bare text in markdown format, or as complex as full blown html pages with svg graphics, embedded objects, interactive javascript and HTML5 visualizations. 
+
+There are several JavaScript slideshow generators that let you compile images and text into a single html file and then scroll between frames. These don't fare well if you have multiple slides with complex interactive visualizations - especially ones that load a lot of data - because all slides get loaded in the background at once. Slider steps out of the way by essentially making each slide a stand-alone html file that is simply linked to the previous and next slides. In fact it leaves your original source files untouched. You can make slides as simple as you want (bare text or an image) but also scale up to as complex as you want as well. The only restriction is the main source files (images, .md files or .html files) need to be in the same directory. 
+
+Usage
+-----
+
+Slider accepts a text file (default: slider.py) with a simple list of slides and generates a linked slideshow from them. The slide list can contain comment lines starting with `#` and blank lines along with the ordered list of files to be linked in the slideshow. An example:
+
+		# Introduction
+		title_graphic.jpg
+		intro_slide.md
+
+		# Main Argument
+		interactive_d3_slide.html
+		data_graph.png
+		protovis_slide.html
+
+		# conclusion
+		bullet_list_summary.md
+		final_slide.md
+		closing_graphic.jpg
+
+Putting this in a file called `slider.txt` in the same directory as these files and calling `slider.py` will generate a set of files named `slide_001.html` through `slide_008.html`. The originals will be left untouched. A stylesheet called `slider.css` will also be generated and you can modify that to change the look of the slideshow.
+
+Individual slides can be in one of te following formats:
+
+* [Markdown](http://daringfireball.net/projects/markdown/syntax) files - you need to have either [Markdown.pl](http://daringfireball.net/projects/markdown/) or [pandoc](http://johnmacfarlane.net/pandoc/) installed on your system.
+* Image - currently .jpg, .gif, and .png are supported.
+* Html file - this can be any html file including scripts, interactive visualizations, embedded objects etc. (Note: _The files need to be fully XML compliant._)
+
+To modify the names of the list or css files or to change the prefix of the slide files, use the following options:
+
+		usage: slider.py [-h] [-s SLIDES] [-c CSS] [-n SLIDENAME]
+
+		optional arguments:
+		  -h, --help            					show this help message and exit
+		  -s SLIDES, --slides SLIDES 				Text file containing a list of slides (default: slider.txt).
+		  -c CSS, --css CSS     					Stylesheet for the slides (default: slider.css).
+		  -n SLIDEPREFIX, --slideprefix SLIDEPREFIX	Name prefix for the slide files (default: slider - generates slider_001.html etc).
+
+
+
+Copyright (C) 2012 Anupam Basu <prime.lens@gmail.com>
--- src/slider.py
+++ src/slider.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-__author__ = 'Anupam Basu (prime.lens@gmail.com)'
-__version__ = '.5' 
+# Copyright (C) 2012 Anupam Basu <prime.lens@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import xml.etree.cElementTree as ET
+""" Generates linked slideshow from a list of files."""
+
+__author__ = 'Anupam Basu'
+__email__ = 'prime.lens@gmail.com'
+__copyright__ = 'Anupam Basu'
+__license__ = 'GPL'
+__version__ = '3.0' 
+
+
+try:
+	import xml.etree.cElementTree as ET
+except ImportError:
+	import xml.etree.ElementTree as ET
+
+from xml.dom.minidom import parseString
 import re
 import os
 from subprocess import Popen, PIPE
@@ -14,8 +40,11 @@ def build_slides(slidelist, css, slidename_prefix):
 	"""Read the list of slides and process the files into a slideshow."""
 
 	if not os.path.isfile(slidelist):
-		print("Can't file the slide list. Please make sure the file exists.")
+		print("Can't file the slide list. Please make sure the file exists. Type `slider --help` for a complete list of options.")
 		exit(0)
+
+	if not re.search(r'\.css$', css):			# make sure css file has proper extension
+		css = css + '.css'
 	
 	slides = open(slidelist, "r").readlines()
 	# Handle newlines, blank lines etc - # begins a comment
@@ -24,16 +53,18 @@ def build_slides(slidelist, css, slidename_prefix):
 	print("Processing %i slides." % (len(slides)))
 
 	for number, slide in enumerate(slides):
-		parse_slide(slide, slidename_prefix, number, len(slides))
+		parse_slide(slide, slidename_prefix, number, len(slides), css)
 		# parse_slide(slide, previous, next, number + 1)
 
 	if os.path.isfile(css):
 		overwrite = raw_input('The CSS stylesheet already exists. Overwrite? (Y/N):').lower().strip()
 		if overwrite == 'y':
 			write_css(css)
+	else:
+		write_css(css)
 
 ################################################################################
-def parse_slide(slide_file, slidename_prefix, slide_no, no_of_slides):
+def parse_slide(slide_file, slidename_prefix, slide_no, no_of_slides, css_file):
 	"""Parse individual slides and insert the link nodes"""
 	
 	# Does the file exist?
@@ -94,7 +125,7 @@ def parse_slide(slide_file, slidename_prefix, slide_no, no_of_slides):
 	# Link the stylesheet
 	head = root.find('head')
 	css = ET.SubElement(head, 'link')
-	css.attrib['href'] = 'slider.css'
+	css.attrib['href'] = css_file
 	css.attrib['rel'] = 'stylesheet'
 	css.attrib['type'] = 'text/css'
 
@@ -106,6 +137,10 @@ def parse_slide(slide_file, slidename_prefix, slide_no, no_of_slides):
 		div1.text, body.text = body.text, ''
 		div1.tail, body.tail = body.tail, ''
 	body.append(div1)
+
+	root_string = re.sub(r'\n\s*\n', r'', ET.tostring(root))
+	root_string = parseString(root_string).toprettyxml(encoding = 'utf-8')
+	root = ET.fromstring(root_string)
 
 	ET.ElementTree(root).write(current_name, method = 'html')
 	print("Processed slide: %s" % (slide_file))
@@ -136,19 +171,23 @@ def markdown_to_html(md_file):
 	html_output = '<html><head><title>' + md_file + '</title></head><body class="markdown_slide"><table id="slider_table"><tr><td>' + html_output + '</td></tr></table></body></html>'
 	return html_output
 
-
 ################################################################################
 def write_css(css):
 	"""Write the CSS file to style the slideshow."""
 
 	css_string = ("body { background:black; color:darkgray; font-family:'Unna', baskerville, garamond, times, times new roman, serif; text-align:left; }"
 					".slider { margin:0px auto; width: 1000px; }"
-					"#slider_next { float:right; }"
-					"#slider_prev { float:left; }"
-					"#slider_next a, #slider_prev a { color:lightgray; font-size:30pt; text-decoration:none; }"
+					"#slider_next { float:right; position:absolute; right:0; top:50%; }"
+					"#slider_prev { float:left; position:absolute; left:0; top:50%; }"
+					"#slider_next a, #slider_prev a { color:#2f2f2f; font-size:30pt; text-decoration:none; }"
 					"img#slider { border:0px solid black; display:block; margin-left:auto; margin-right:auto; width:1000px; box-shadow:4px 4px 8px #000; }"
 					"#slider_table { height:100%; width:100%; }"
 					"#slider_table td { vertical-align: middle; }")
+
+	# Format the css file
+	css_string = re.sub(r'({|}|;)', r'\1\n', css_string)
+	css_string = re.sub(r'(})', r'\1\n', css_string)
+	css_string = re.sub(r'\n (\S)', r'\n\t\1', css_string)
 
 	file = open(css, "w")
 	file.write(css_string)
@@ -160,11 +199,11 @@ def write_css(css):
 if __name__ == "__main__":
 	import argparse
 
-	parser = argparse.ArgumentParser(description = 'Connect html documents into a linked set of slides')
-	parser.add_argument('-s', '--slides', action = 'store', nargs = 1, default = 'slider.txt', help = 'List of slides.')
-	parser.add_argument('-c', '--css', action = 'store', nargs = 1, default = 'slider.css', help = 'Stylesheet for the slides.')
-	parser.add_argument('-n', '--slidenameprefix', action = 'store', nargs = 1, default = 'slider', help = 'Names of the slide files.')
+	parser = argparse.ArgumentParser(description = 'Connect images, markdown files, and html documents into a linked slideshow.')
+	parser.add_argument('-s', '--slides', action = 'store', nargs = 1, default = ['slider.txt'], help = 'Text file containing a list of slides (default: slider.txt).')
+	parser.add_argument('-c', '--css', action = 'store', nargs = 1, default = ['slider.css'], help = 'Stylesheet for the slides (default: slider.css).')
+	parser.add_argument('-n', '--slideprefix', action = 'store', nargs = 1, default = ['slider'], help = 'Name prefix for the slide files (default: slider - generates slider_001.html etc.)')
 
 	args = parser.parse_args()
-	build_slides(args.slides, args.css, args.slidenameprefix)
+	build_slides(args.slides[0], args.css[0], args.slideprefix[0])
 
